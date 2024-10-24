ec192c6f989a3b7d4685950d277c8ef15394d41a
dkontek <dkontek@RA117B-24>
2024-10-24 10:38:59 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 10;
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+        }
+    }
+}
