e4eed632c9094c037dc2d39bb7bbaac0fc693534
Damian Kontek <dkontek@wsiz.edu.pl>
2024-11-14 10:39:09 +0100

    DFFD

---
+++ Lab2/Models/Person.cs
@@ -0,0 +1,55 @@
+﻿
+
+namespace Lab2.Models
+{
+    public class Person
+    {
+        private string name;
+        private string surname;
+        private int age;
+        private string pesel;
+
+        public string Name { get { return name; } set { name = value; } }
+        public string Surname { get { return surname; } set { surname = value; } }
+        public int Age { get { return age; } 
+            set {
+                if (value > 0) age = value;
+                else age = 0;
+            } }
+        public string Pesel => pesel;// { get { return pesel; } }
+
+        public Person() {
+            name = "Test";
+        }
+
+        public Person(string name) { 
+            this.name = name;   
+        }
+
+        public void Welcome()
+        {
+            Console.WriteLine("Witaj");
+        }
+
+        public void Welcome(string otherName)
+        {
+            Console.WriteLine($"Witaj {otherName} Jestem {Name}");
+        }
+
+        public void Welcome(int otherName)
+        {
+            Console.WriteLine($"Witaj {otherName} Jestem {Name}");
+        }
+
+
+        public static Person Create()
+        {
+            return new Person();
+        }
+
+        public static Person MapUser(User n)
+        {
+            return new Person(n.UserName);
+        }
+    }
+}
---
+++ Lab2/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lab2.Models;
+
+Console.WriteLine("Hello, World!");
+
+
+Person os = new Person();
+
+os.Welcome();
+os.Welcome("DD");
+
+var n = new User { UserName = "Test" };
+n.UserName = "Test2";
+
+
+var os2 = Person.MapUser(n);
+
+
+Console.WriteLine(os2.Name);
