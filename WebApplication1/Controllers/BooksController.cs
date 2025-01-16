8ce099e44ddc8f4f4475116866f005c0722bdcee
Damian Kontek <dkontek@wsiz.edu.pl>
2025-01-16 10:47:45 +0100

    .

---
+++ WebApplication1/Controllers/BooksController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Lab2.Lab6;
+using Lab2.Interfaces;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class BooksController(IBookRepository repository) : ControllerBase
+    {
+
+        [HttpGet]
+        public List<Book> GetAll()
+        {
+            return repository.GetAll();
+        }
+
+        [HttpGet]
+        public ActionResult<Book> Get(int id)
+        {
+            return Ok(repository.Get(id));
+        }
+
+
+        [HttpPost]
+        public void Create(Book value)
+        {
+            repository.Create(value);
+        }
+
+        [HttpPut]
+        public void Update(Book value)
+        {
+            repository.Update(value);
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            repository.Delete(id);
+        }
+
+        [HttpGet]
+        public List<Book> GetBookByAuthors(string author)
+        {
+            return repository.GetBooksByAuthor(author);
+        }
+
+        [HttpGet]
+        public List<Book> GetByYear(int year)
+        {
+            return repository.GetBooksByPublishYear(year);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/PersonController.cs
@@ -0,0 +1,63 @@
+﻿using Lab2.Interfaces;
+using Lab2.Lab6;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly IPersonRepository _repository;
+
+        public PersonController(IPersonRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        [HttpGet]
+        public List<Person> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        [HttpGet]
+        public ActionResult<Person> Get(int id)
+        {
+            return Ok(_repository.Get(id));
+        }
+
+        [HttpPost]
+        public void Create(Person value)
+        {
+            _repository.Create(value);
+        }
+
+        [HttpPut]
+        public void Update(Person value)
+        {
+            _repository.Update(value);
+        }
+
+        [HttpDelete]
+        public void Delete(int id)
+        {
+            _repository.Delete(id);
+        }
+
+        [HttpGet]
+        public List<Book> GetBoorowedBooks(int id)
+        {
+            return _repository.GetBorrowedBook(id);
+        }
+
+        [HttpPost]
+
+        public void BorrwBook(int id, Book book)
+        {
+            _repository.BorrowBook(id, book);
+        }
+    }
+}
